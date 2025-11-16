b7f8bc05428904ba859a6af5c76df7568bec90d3
VictoriaLafMicaela <103388428+VictoriaLafMicaela@users.noreply.github.com>
2025-11-16 11:51:07 -0300

    Create Planta.java
    Cree la clase planta con los atributos nombre y tipo

---
+++ app/src/main/java/com/plantcare/Planta.java
@@ -0,0 +1,9 @@
+public class Planta {
+    public String nombre;
+    public String tipo;
+
+    public Planta(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+}
