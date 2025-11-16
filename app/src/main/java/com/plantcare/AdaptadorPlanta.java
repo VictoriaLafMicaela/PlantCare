a212fd6b17d521ab874ed59e641994d5cbff4cbc
VictoriaLafMicaela <103388428+VictoriaLafMicaela@users.noreply.github.com>
2025-11-16 11:50:09 -0300

    Create AdaptadorPlanta.java

---
+++ app/src/main/java/com/plantcare/AdaptadorPlanta.java
@@ -0,0 +1,69 @@
+package com.example.plantcare;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdaptadorPlanta extends RecyclerView.Adapter<AdaptadorPlanta.PlantaViewHolder> {
+
+    private List<Planta> listaPlantas;
+
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    private OnItemClickListener listener;
+
+
+    public AdaptadorPlanta(List<Planta> listaPlantas, OnItemClickListener listener) {
+        this.listaPlantas = listaPlantas;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public PlantaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_planta, parent, false);
+        return new PlantaViewHolder(vista, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlantaViewHolder holder, int position) {
+        Planta p = listaPlantas.get(position);
+        holder.txtNombre.setText(p.nombre);
+        holder.txtTipo.setText(p.tipo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPlantas != null ? listaPlantas.size() : 0;
+    }
+
+    static class PlantaViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtNombre;
+        TextView txtTipo;
+
+        public PlantaViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+
+            txtNombre = itemView.findViewById(R.id.txtNombrePlanta);
+            txtTipo = itemView.findViewById(R.id.txtTipoPlanta);
+
+
+            itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    listener.onItemClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+}
