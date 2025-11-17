59a0cb2c9a788c89564f396893e31217bbd465f9
VictoriaLafMicaela <103388428+VictoriaLafMicaela@users.noreply.github.com>
2025-11-16 21:59:15 -0300

    Update Vista1Activity.java
    lista se actualiza, botones funcionales

--- app/src/main/java/com/example/plantcare/Vista1Activity.java
+++ app/src/main/java/com/example/plantcare/Vista1Activity.java
@@ -19,6 +19,7 @@ public class Vista1Activity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private AdaptadorPlanta adaptadorPlantas;
+    private PlantaDAO plantaDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +29,50 @@ public class Vista1Activity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         SharedPreferences sp = getSharedPreferences(PREFS, MODE_PRIVATE);
         String nombre = sp.getString(KEY_NOMBRE, "Usuario");
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Bienvenido, " + nombre);
         }
 
-
+        plantaDAO = AppDatabase.getInstance(this).plantaDAO();
         recyclerView = findViewById(R.id.recyclerViewTextos);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        List<Planta> plantas = plantaDAO.todas();
 
-        List<Planta> plantas = PlantLista.obtenerPlantas();
-
-
-        AdaptadorPlanta adaptadorPlantas = new AdaptadorPlanta(
+        adaptadorPlantas = new AdaptadorPlanta(
                 plantas,
                 position -> {
+                    int id = adaptadorPlantas.listaPlantas.get(position).id;
                     Intent i = new Intent(Vista1Activity.this, EditarPlantaActivity.class);
-                    i.putExtra("posicion", position);
-
+                    i.putExtra("id_planta", id);
                     startActivity(i);
                 }
         );
 
         recyclerView.setAdapter(adaptadorPlantas);
 
-
-
+        Button btnMisPlantas = findViewById(R.id.btnMisPlantas);
+        Button btnGaleria = findViewById(R.id.btnGaleria);
         Button btnAgregar = findViewById(R.id.btnAgregarPlanta);
         Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
 
+        btnMisPlantas.setOnClickListener(v -> {
+            List<Planta> listaActual = plantaDAO.todas();
+            adaptadorPlantas.actualizarLista(listaActual);
+        });
+
+        btnGaleria.setOnClickListener(v -> {
+            Intent intent = new Intent(Vista1Activity.this, GaleriaActivity.class);
+            startActivity(intent);
+        });
 
         btnAgregar.setOnClickListener(v -> {
             Intent i = new Intent(Vista1Activity.this, AgregarPlantaActivity.class);
             startActivity(i);
         });
-
 
         btnCerrarSesion.setOnClickListener(v -> {
             sp.edit().clear().apply();
@@ -78,9 +85,7 @@ public class Vista1Activity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        if (recyclerView.getAdapter() != null) {
-            recyclerView.getAdapter().notifyDataSetChanged();
-        }
+        List<Planta> plantasActualizadas = plantaDAO.todas();
+        adaptadorPlantas.actualizarLista(plantasActualizadas);
     }
 }
